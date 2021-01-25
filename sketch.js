2d72c1d65b6a459a0356ee9a1c9ed156e9103aea
Aatreyee <70950558+Aatreyee@users.noreply.github.com>
2021-01-25 18:37:58 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,69 @@
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+const Body = Matter.Body;
+
+var k=0;
+var bridge;
+var logs=[];
+var bridgeScene;
+var bridgeGirl,bridgeGirlAni,bridgeGirlSprite;
+
+var world,engine;
+function preload()
+{
+	bridgeGirlAni = loadAnimation("sprite000.png","sprite001.png","sprite002.png","sprite003.png","sprite004.png","sprite005.png");
+	bridgeSceneImg = loadImage("bridgeScene.png");
+}
+
+function setup() {
+	
+	createCanvas(windowWidth,windowHeight);
+	engine = Engine.create();
+	world = engine.world;
+	 
+	for(var y=100;y<height-100;y=y+50){
+		logs.push(new Bridge(width/2-100,y,300,20))
+	}
+
+	bridgeGirl=Bodies.rectangle(width/2,height-200,100,140);
+	World.add(world,bridgeGirl);
+
+	bridgeScene = createSprite(0,width);
+	bridgeScene.addImage(bridgeSceneImg);
+	
+	bridgeGirlSprite = createSprite(100,height-200);
+	bridgeGirlSprite.addAnimation("girl",bridgeGirlAni);
+
+	Engine.run(engine);
+  
+}
+
+
+function draw() {
+  
+  background(bridgeSceneImg);
+
+  for(var l=0;l<logs.length;l=l+1){
+	  logs[l].display();
+  }
+
+  if(frameCount%100===0){
+	  Matter.Body.setStatic(logs[k].body,false);
+	  k=k+1;
+  }
+  if(keyDown(UP_ARROW)){
+	bridgeGirl.y = bridgeGirl.y-10;
+  }
+  
+  
+
+  bridgeGirlSprite.x=bridgeGirl.position.x;
+  bridgeGirlSprite.y=bridgeGirl.position.y;
+  
+  drawSprites();
+ 
+}
+
+
+
